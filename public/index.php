ad09712329eb831ce15ad7c114477c5134299fb8
Byron <bgva2005@yahoo.com>
2016-10-16 17:01:05 -0500

    Revision correcta importacion archivos

--- public/index.php
+++ public/index.php
@@ -1,5 +1,30 @@
-pllantilla_mvc
 <?php
+
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', realpath(dirname(__FILE__)) . DS);
+define('APP_PATH', ROOT . 'application' . DS);
+
+
+require_once APP_PATH . 'Config.php';
+require_once APP_PATH . 'Request.php';
+require_once APP_PATH . 'Bootstrap.php';
+require_once APP_PATH . 'Controller.php';
+require_once APP_PATH . 'Model.php';
+require_once APP_PATH . 'View.php';
+require_once APP_PATH . 'Registro.php';
+
+echo '<pre>'; print_r(get_required_files());
+
+
+
+exit();
+echo '<br>';
+echo 'usando SERVER ' . $_SERVER['REQUEST_URI'] . '<br>';
+echo $_GET['url'];
+
+//phpinfo();
+//exit();
 echo "\n".'$_GET'."\n"; var_dump($_GET);
 echo  '<br>';
 echo '<pre>';
