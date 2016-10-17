27f0c2c6637da8c627f0b06beb0c75d6edcfbaea
Byron <bgva2005@yahoo.com>
2016-10-16 20:44:36 -0500

    verificar llamada a indexController

--- public/application/Bootstrap.php
+++ public/application/Bootstrap.php
@@ -7,7 +7,29 @@ class Bootstrap
 		$controller = $peticion->getControlador() . 'Controller';
 		$rutaControlador = ROOT . 'controllers' . DS . $controller . '.php';
 
-		echo $rutaControlador; exit;
+		$metodo =  $peticion->getMetodo();
+		$args = $peticion->getArgs();
+
+		if(is_readable($rutaControlador)){
+			//vverificar si archivo existe y es legible
+			require_once $rutaControlador;
+
+			$controller = new $controller;
+
+			if(is_callable(array($controller, $metodo))){
+				$metodo = $peticion->getMetodo();
+			} else {
+				$metodo = DEFAULT_METODO;
+			}
+
+			if(isset($args)){
+				call_user_func_array(array($controller, $metodo), $args);
+			} else {
+				call_user_func(array($controller, $metodo));
+			}
+		} else {
+			throw new Exception('Error en Bootstrap: No encontrado');
+		}
 
 	}
 }
--- public/controllers/indexController.php
+++ public/controllers/indexController.php
@@ -4,6 +4,11 @@ class indexController extends Controller
 {
 	public function __construct(){
 		//parent::__construct();
+		
+	}
+
+	public function index()
+	{
 		echo 'hola desde index controler ';
 	}
 
