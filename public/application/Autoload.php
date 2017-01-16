6c78e858f926475c687a24a4100ace2706a72114
Byron <bgva2005@yahoo.com>
2017-01-16 14:04:50 -0500

    No se implementa video 23 a partir 1008

--- public/application/Autoload.php
+++ public/application/Autoload.php
@@ -6,8 +6,15 @@ function autoloadCore($class)
 	{
 		include_once APP_PATH . ucfirst(strtolower($class)) . '.php';
 	}
+}
 
-	
+function autoloadLibs($class)
+{
+	if(file_exists (ROOT . 'libs' . DS . 'class' . strtolower($class) . '.php'))
+	{
+		include_once ROOT . 'libs' . DS . 'class' . strtolower($class) . '.php';
+	}
 }
 
 spl_autoload_register('autoloadCore');
+//spl_autoload_register('autoloadLibs');
